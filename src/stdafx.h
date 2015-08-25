d01d43fa609dad033129bc9575794e76be08f837
unknown <rwb@Dev-Intrn-3.CNCSOFTWARE.com>
2015-08-25 15:48:29 -0400

    Added several files for the plotter, like plane, point, and line. Need to think if it should be generalized more. Will add more tonight hopefully.

---
+++ src/stdafx.h
@@ -0,0 +1,10 @@
+#include <list>
+#include <memory>
+#include <math.h>
+#include <string>
+
+#include "cinder/app/AppNative.h"
+#include "cinder/gl/gl.h"
+#include "cinder/MayaCamUI.h"
+
+#include "GUI/GUI.h"
